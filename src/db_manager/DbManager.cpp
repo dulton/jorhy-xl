4856d62da1ccc03e2ce52d6dfb7506d21f8be40d
joorhy <joorhy@gmail.com>
2013-09-18 17:41:40 +0800

    modified:   src/db_manager/DbManager.cpp 	modified:   src/dev_manager/DeviceManager.cpp 	modified:   src/nvr.cpp 	modified:   vs2012/dev_manager.vcxproj

--- src/db_manager/DbManager.cpp
+++ src/db_manager/DbManager.cpp
@@ -1,11 +1,18 @@
-/** 
-*  Copyright (c) 2013, xx科技有限公司 
-*  All rights reserved. 
-* 
-*  @file        DbManager.cpp 
-*  @author   Jorhy(joorhy@gmail.com) 
-*  @date      2013/09/18 15:30 
-*/  
+///////////////////////////////////////////////////////////////////////////
+/// COPYRIGHT NOTICE
+/// Copyright (c) 2009, xx科技有限公司(版权声明) 
+/// All rights reserved. 
+/// 
+/// @file      DbManager.cpp 
+/// @brief     数据库管理模块实现
+///
+/// @version   1.0 (版本声明)
+/// @author    Jorhy (joorhy@gmail.com) 
+/// @date      2013/09/18 17:07 
+///
+///
+/// 修订说明：最初版本
+///////////////////////////////////////////////////////////////////////////  
 #include "DbManager.h"
 
 CDbManager::CDbManager()
--- src/nvr.cpp
+++ src/nvr.cpp
@@ -1,11 +1,18 @@
-/** 
-*  Copyright (c) 2013, xx科技有限公司 
-*  All rights reserved. 
-* 
-*  @file        nvr.cpp 
-*  @author      Jorhy(joorhy@gmail.com) 
-*  @date        2013/09/18 15:25 
-*/  
+///////////////////////////////////////////////////////////////////////////
+/// COPYRIGHT NOTICE
+/// Copyright (c) 2009, xx科技有限公司(版权声明) 
+/// All rights reserved. 
+/// 
+/// @file      nvr.cpp 
+/// @brief     NVR应用程序模块
+///
+/// @version   1.0 (版本声明)
+/// @author    Jorhy (joorhy@gmail.com) 
+/// @date      2013/09/18 17:11 
+///
+///
+/// 修订说明：最初版本
+///////////////////////////////////////////////////////////////////////////  
 
 int main(int argc, char **argv)
 {
